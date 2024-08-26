3d32b3b106159209d4e7c843e9b3d2c5221d5bf0
tmz81 <tmz.zt4@gmail.com>
2024-08-26 10:55:07 -0300

    fix layout

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,81 +1,88 @@
-import { Container, Box, Typography, Grid } from "@mui/material";
-import MiniMap from "./components/MiniMap";
-
+import React from "react";
+import { Box, Container, Typography, Grid } from "@mui/material";
+import { LocationOn, Email, Phone } from "@mui/icons-material";
+import MiniMapLocation from "./components/MiniMap";
+import logo from "../assets/logo-mineralab.png"
 export default function Footer() {
   return (
-    <Box
-      id="footer"
-      component="footer"
-      sx={{
-        backgroundColor: "#ffefd5",
-        padding: "50px 0",
-        color: "#1E2125",
-        pt: "78px",
-      }}
-    >
-      <Container maxWidth="lg" justifyContent="space-beetwen">
-        <Box alignContent="center" justifyContent="center" alignItems="center">
-          <Grid container>
-            <Grid item xs={12} md={4}>
+    <Box sx={{ bgcolor: "#ffefd5", color: "#1E2125", py: 4 }}>
+      <Container maxWidth="lg">
+        <Grid container spacing={4}>
+          <Grid item xs={12} md={4}>
+            <Typography variant="h6" gutterBottom>
               <Box
+                component="a"
+                href="#"
                 sx={{
                   display: "flex",
-                  flexDirection: "column",
-                  padding: "20px",
-                  gap: "8px",
-                  borderBottom: "1px solid #1E2125",
-                  pb: 5,
+                  mr: 2,
+                  fontFamily: "Montserrat",
+                  caretColor: "transparent",
+                  fontWeight: 700,
+                  color: "inherit",
+                  textDecoration: "none",
                 }}
               >
-                <MiniMap />
-              </Box>
-            </Grid>
-            <Grid item xs={12} md={4} sx={{ textAlign: "center" }}>
-              <Box
-                sx={{
-                  display: "flex",
-                  flexDirection: "column",
-                  padding: "50px",
-                  gap: "8px",
-                  borderBottom: "1px solid #1E2125",
-                  pb: 5,
-                }}
-              >
-                <Typography variant="body2">
-                  O Laboratório de Mineralogia do Solo (MineraLAB) pertence à
-                  Área de Solos do Departamento de Agronomia da Universidade
-                  Federal Rural de Pernambuco (campus Dois Irmãos, Recife), e é
-                  integrante do Programa de Pós-graduação em Ciência do Solo
+                <Box
+                  component="img"
+                  src={logo}
+                  alt="logo"
+                  sx={{
+                    height: 60,
+                    userSelect: "default",
+                    cursor: "default",
+                    caretColor: "transparent",
+                  }}
+                />
+                <Typography variant="h6" gutterBottom fontWeight={400}>
+                  Laboratório de Mineralogia do Solo
                 </Typography>
               </Box>
-            </Grid>
-            <Grid item xs={12} md={4} sx={{ textAlign: "center" }}>
-              <Box
-                sx={{
-                  display: "flex",
-                  flexDirection: "column",
-                  padding: "20px",
-                  gap: "8px",
-                  borderBottom: "1px solid #1E2125",
-                  pb: 5,
-                }}
-              >
-                <Typography variant="h6">Contato</Typography>
-                <Typography variant="body2">
-                  Responsável: Valdomiro Severino de Souza Júnior
-                </Typography>
-                <Typography variant="body2">
-                  E-mail: mineralab.ufrpe@gmail.com
-                </Typography>
-                <Typography variant="body2">
-                  Telefone: (81) 9-8808-8183
-                </Typography>
-              </Box>
-              <Typography variant="body2" sx={{ fontSize: "14px", pt: 4 }}>
-                MineraLab © 2024. All rights reserved.
-              </Typography>
-            </Grid>
+            </Typography>
+            <Typography variant="body1" gutterBottom>
+              O (MineraLAB) pertence à Área
+              de Solos do Departamento de Agronomia da Universidade Federal
+              Rural de Pernambuco (campus Dois Irmãos, Recife), e é integrante
+              do Programa de Pós-graduação em Ciência do Solo.
+            </Typography>
           </Grid>
+          <Grid item xs={12} md={4}>
+            <Typography variant="h6" gutterBottom>
+              Contato
+            </Typography>
+            <Box sx={{ display: "flex", alignItems: "center", mb: 1 }}>
+              <LocationOn sx={{ mr: 1 }} />
+              <Typography>
+                Rua Dom Manuel de Medeiros, s/n, Dois Irmãos, Recife/ PE,
+                52.171-900.
+              </Typography>
+            </Box>
+            <Box sx={{ display: "flex", alignItems: "center", mb: 1 }}>
+              <Email sx={{ mr: 1 }} />
+              <Typography>mineralab.ufrpe@gmail.com</Typography>
+            </Box>
+            <Box sx={{ display: "flex", alignItems: "center" }}>
+              <Phone sx={{ mr: 1 }} />
+              <Typography>(81) 9-8808-8183</Typography>
+            </Box>
+          </Grid>
+          <Grid item xs={12} md={4}>
+            <Box>
+              <MiniMapLocation />
+            </Box>
+          </Grid>
+        </Grid>
+        <Box
+          sx={{
+            textAlign: "center",
+            mt: 4,
+            pt: 3,
+            borderTop: "1px solid black",
+          }}
+        >
+          <Typography variant="body2">
+            MineraLab © 2024. All rights reserved.
+          </Typography>
         </Box>
       </Container>
     </Box>
